62c1c6dea9cfb8da475a9aa790ca9ac39a14c5f6
Santiago Di Camillo <dicamillosantiago@gmail.com>
2023-06-27 16:23:03 -0300

    Agrego LogIn y redondeado de texto

---
+++ app/src/main/java/andoroid/app/mobiles/notimed/LogIn.kt
@@ -0,0 +1,11 @@
+package andoroid.app.mobiles.notimed
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+    }
+}
