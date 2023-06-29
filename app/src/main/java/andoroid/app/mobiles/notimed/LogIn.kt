d19386b80d7f5b7716bac874a58f1fdc35fef16f
Santiago Di Camillo <dicamillosantiago@gmail.com>
2023-06-29 16:40:35 -0300

    Agrega log out y autentificación con Google

--- app/src/main/java/andoroid/app/mobiles/notimed/LogIn.kt
+++ app/src/main/java/andoroid/app/mobiles/notimed/LogIn.kt
@@ -1,21 +1,50 @@
 package andoroid.app.mobiles.notimed
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+
 
 class LogIn : AppCompatActivity() {
+
+    private val GOOGLE_SIGN_IN = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
 
         setup()
+        sesion()
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val layout = findViewById<RelativeLayout>(R.id.logInLayout)
+        layout.visibility = View.VISIBLE
+    }
+
+    private fun sesion(){
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)
+        val email = prefs.getString("email", null)
+        if (email != null){
+            val layout = findViewById<RelativeLayout>(R.id.logInLayout)
+            layout.visibility = View.INVISIBLE
+            showPerfil(email)
+        }
     }
 
     private fun setup(){
@@ -23,6 +52,7 @@ class LogIn : AppCompatActivity() {
         val email = findViewById<EditText>(R.id.email).text
         val pass = findViewById<EditText>(R.id.contra).text
         val registroBtn = findViewById<TextView>(R.id.registrate)
+        val googleBtn = findViewById<Button>(R.id.googleBtn)
 
         //Inicio de sesión
         btn.setOnClickListener{
@@ -36,6 +66,14 @@ class LogIn : AppCompatActivity() {
                     }
                 }
             }
+        }
+
+        //configuración de google
+        googleBtn.setOnClickListener {
+            val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build()
+            val googleClient = GoogleSignIn.getClient(this, googleConf)
+            googleClient.signOut()
+            startActivityForResult(googleClient.signInIntent, GOOGLE_SIGN_IN)
         }
 
         //Registrarse si no tiene cuenta
@@ -60,5 +98,29 @@ class LogIn : AppCompatActivity() {
             putExtra("email", email)
         }
         startActivity(perfil)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GOOGLE_SIGN_IN){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+
+                if (account != null){
+                    val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+                    FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            showPerfil(account.email ?: "")
+
+                        } else {
+                            showAlert()
+                        }
+                    }
+                }
+            } catch (e: ApiException){
+                showAlert()
+            }
+        }
     }
 }
--- app/src/main/java/andoroid/app/mobiles/notimed/Perfil.kt
+++ app/src/main/java/andoroid/app/mobiles/notimed/Perfil.kt
@@ -1,5 +1,6 @@
 package andoroid.app.mobiles.notimed
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -14,10 +15,16 @@ class Perfil : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.perfil)
 
-
+        //SETUP
         val bundle = intent.extras
         val email = bundle?.getString("email")
         setup(email ?: "")
+
+        //guardado de datos
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+        prefs.putString("email", email)
+        prefs.apply()
+
     }
 
     private fun setup(email:String){
@@ -26,7 +33,14 @@ class Perfil : AppCompatActivity() {
 
         val btn = findViewById<Button>(R.id.logOutBtn)
 
+        //cerrar sesión
         btn.setOnClickListener {
+
+            //borrado de datos de sesión
+            val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+            prefs.clear()
+            prefs.apply()
+
             FirebaseAuth.getInstance().signOut()
             showLogIn()
         }
