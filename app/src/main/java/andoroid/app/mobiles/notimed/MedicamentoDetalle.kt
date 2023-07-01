f4612bec48056c7f9eec57ac8cf1dbdf9f6fb05d
Santiago Di Camillo <dicamillosantiago@gmail.com>
2023-07-01 15:24:36 -0300

    Agrego vista del detalle

--- app/src/main/java/andoroid/app/mobiles/notimed/MedicamentoDetalle.kt
+++ app/src/main/java/andoroid/app/mobiles/notimed/MedicamentoDetalle.kt
@@ -1,4 +1,32 @@
 package andoroid.app.mobiles.notimed
 
-class MedicamentoDetalle {
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MedicamentoDetalle : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.detalle_medicamentos)
+
+        //SETUP
+        setup()
+    }
+
+    private fun setup(){
+
+        //variables desde el item medicamento
+        val name = intent.getStringExtra("name")
+        val dosis = intent.getStringExtra("dosis")
+        val stock = intent.getStringExtra("stock")
+
+        //controles
+        val nameView = findViewById<TextView>(R.id.nombreDetalle)
+        val dosisView = findViewById<TextView>(R.id.dosisDetalle)
+        val stockView = findViewById<TextView>(R.id.stockDetalle)
+
+        nameView.text = name
+        dosisView.text = dosis
+        stockView.text = stock
+    }
 }
