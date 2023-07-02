e3bdaa501557b363a7c63b321b163fe8aef3d329
Santiago Di Camillo <dicamillosantiago@gmail.com>
2023-07-02 18:03:54 -0300

    Agrego pantalla de lista de medicamentos y su funcionalidad

---
+++ app/src/main/java/andoroid/app/mobiles/notimed/ListaMedicamentoAdapter.kt
@@ -0,0 +1,52 @@
+package andoroid.app.mobiles.notimed
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ListaMedicamentoAdapter(private val medicamentos: List<Medicamento>) : RecyclerView.Adapter<ListaMedicamentoAdapter.MedicamentoViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MedicamentoViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.medicamento, parent, false)
+        return MedicamentoViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MedicamentoViewHolder, position: Int) {
+        val medicamento = medicamentos[position]
+
+        holder.bind(medicamento)
+    }
+
+    override fun getItemCount(): Int {
+        return medicamentos.size
+    }
+
+    inner class MedicamentoViewHolder(itemview: View) : RecyclerView.ViewHolder(itemview) {
+        val nombreTextView: TextView = itemView.findViewById(R.id.nombreMed)
+        val dosisTextView: TextView = itemView.findViewById(R.id.dosisMed)
+        val stockTextView: TextView = itemView.findViewById(R.id.stockMed)
+
+
+        fun bind(medicamento: Medicamento){
+            nombreTextView.text = medicamento.name
+            dosisTextView.text = medicamento.dosis
+            stockTextView.text = medicamento.stock
+
+            //abre el detalle del medicamento
+            itemView.setOnClickListener{
+                val context = itemView.context
+                val intent = Intent(context, MedicamentoDetalle::class.java)
+                intent.putExtra("id", medicamento.id)
+                intent.putExtra("name", medicamento.name)
+                intent.putExtra("dosis", medicamento.dosis)
+                intent.putExtra("stock", medicamento.stock)
+                context.startActivity(intent)
+            }
+
+        }
+    }
+
+}
---
+++ app/src/main/java/andoroid/app/mobiles/notimed/ListaMedicamentos.kt
@@ -0,0 +1,69 @@
+package andoroid.app.mobiles.notimed
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+class ListaMedicamentos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_medicamentos)
+
+        obtenerMedicamentos()
+    }
+
+    private lateinit var recyclerView: RecyclerView
+
+    private fun obtenerMedicamentos() {
+        val database = FirebaseDatabase.getInstance()
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        val uid = currentUser?.uid
+
+        if (uid != null) {
+            val medicamentosRef = database.getReference("users").child(uid).child("medicamentos")
+
+            val medicamentosList = mutableListOf<Medicamento>()
+
+            medicamentosRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    for (medicamentoSnapshot in dataSnapshot.children) {
+
+                        val medId = medicamentoSnapshot.key
+                        val medName = medicamentoSnapshot.child("name").getValue(String::class.java)
+                        val medDosis = medicamentoSnapshot.child("dosis").getValue(String::class.java)
+                        val medStock = medicamentoSnapshot.child("stock").getValue(String::class.java)
+
+
+                        if (medId != null && medName != null && medDosis != null && medStock != null){
+                            val medicamento = Medicamento(medId, medName, medDosis, medStock)
+                            medicamentosList.add(medicamento)
+                        }
+                    }
+
+                    mostrarMedicamentos(medicamentosList)
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    // Manejar el error en caso de que ocurra
+                }
+            })
+
+        }
+    }
+
+    private fun mostrarMedicamentos(medicamentosList: List<Medicamento>){
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val adapter = ListaMedicamentoAdapter(medicamentosList)
+        recyclerView.adapter = adapter
+
+    }
+}
