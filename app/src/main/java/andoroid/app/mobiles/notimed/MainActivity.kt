93561ffd090c0577888163f5e68bb8081f56e336
Santiago Di Camillo <dicamillosantiago@gmail.com>
2023-07-02 20:30:04 -0300

    Función de guardar datos de la sesión movida a la pantalla principal

--- app/src/main/java/andoroid/app/mobiles/notimed/MainActivity.kt
+++ app/src/main/java/andoroid/app/mobiles/notimed/MainActivity.kt
@@ -1,5 +1,6 @@
 package andoroid.app.mobiles.notimed
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -31,8 +32,20 @@ class MainActivity : AppCompatActivity() {
         }
 
         obtenerMedicamentos()
+
+        //guardado de datos de la sesión
+        guardarDatos()
     }
 
+
+    private fun guardarDatos(){
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+
+        val firebaseAuth = FirebaseAuth.getInstance()
+        val email = firebaseAuth.currentUser?.email
+        prefs.putString("email", email)
+        prefs.apply()
+    }
 
     private fun obtenerMedicamentos() {
         val database = FirebaseDatabase.getInstance()
--- app/src/main/java/andoroid/app/mobiles/notimed/Perfil.kt
+++ app/src/main/java/andoroid/app/mobiles/notimed/Perfil.kt
@@ -33,9 +33,6 @@ class Perfil : AppCompatActivity() {
         //SETUP
         setup()
 
-        //guardado de datos de la sesión
-        guardarDatos()
-
     }
 
     private fun setup(){
@@ -106,15 +103,6 @@ class Perfil : AppCompatActivity() {
 
             })
         }
-    }
-
-    private fun guardarDatos(){
-        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
-
-        val firebaseAuth = FirebaseAuth.getInstance()
-        val email = firebaseAuth.currentUser?.email
-        prefs.putString("email", email)
-        prefs.apply()
     }
 
     private fun showLogIn(){
