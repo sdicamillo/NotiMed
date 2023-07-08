c00626481f9e80dc6e40083ad889a26db3f1aed4
santiagof2 <santiagofritz97@gmail.com>
2023-07-08 20:21:19 -0300

    Logica de alarma

---
+++ app/src/main/java/andoroid/app/mobiles/notimed/AlarmReceiver.kt
@@ -0,0 +1,16 @@
+package andoroid.app.mobiles.notimed
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.widget.Toast
+
+class AlarmReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
+        val ringtone = RingtoneManager.getRingtone(context, alarmUri)
+        ringtone.play()
+        Toast.makeText(context, "¡Alarma activada!", Toast.LENGTH_SHORT).show()
+    }
+}
