afa14b51dd3daeb2ae23efe8359e22c2aa9c298f
santiagof2 <santiagofritz97@gmail.com>
2023-07-09 09:20:38 -0300

    No volver a cargar el layout seleccionado del menu si ya se encuentra en el layout

--- app/src/main/java/andoroid/app/mobiles/notimed/BottomNavigationHandler.kt
+++ app/src/main/java/andoroid/app/mobiles/notimed/BottomNavigationHandler.kt
@@ -4,30 +4,45 @@ import android.content.Context
 import android.content.Intent
 import android.view.MenuItem
 import android.app.Activity
+import android.view.View
 
 object BottomNavigationHandler {
-        fun handleNavigationItemSelected(context: Context, item: MenuItem) {
-            val currentActivity = context as Activity
+    fun handleNavigationItemSelected(context: Context, item: MenuItem) {
+        val currentActivity = context as Activity
 
-            when (item.itemId) {
-                R.id.navigation_search -> {
-                    val intent = Intent(context, MainActivity::class.java)
-                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
-                    currentActivity.startActivity(intent)
-                    currentActivity.finishAffinity()
+        when (item.itemId) {
+            R.id.navigation_search -> {
+                if (currentActivity is MainActivity) {
+                    return
                 }
-                R.id.navigation_home -> {
-                    val intent = Intent(context, ListaMedicamentos::class.java)
-                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
-                    currentActivity.startActivity(intent)
-                    currentActivity.finishAffinity()
+
+                val intent = Intent(context, MainActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                currentActivity.startActivity(intent)
+                currentActivity.finishAffinity()
+            }
+            R.id.navigation_home -> {
+                if (currentActivity is ListaMedicamentos) {
+                    return
                 }
-                R.id.navigation_profile -> {
-                    val intent = Intent(context, Perfil::class.java)
-                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
-                    currentActivity.startActivity(intent)
-                    currentActivity.finishAffinity()
+
+                val intent = Intent(context, ListaMedicamentos::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                currentActivity.startActivity(intent)
+                currentActivity.finishAffinity()
+            }
+            R.id.navigation_profile -> {
+                if (currentActivity is Perfil) {
+                    return
                 }
+
+                val intent = Intent(context, Perfil::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                currentActivity.startActivity(intent)
+                currentActivity.finishAffinity()
             }
         }
     }
+}
+
+
