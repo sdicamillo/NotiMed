b77dbaf8a990fc1871de54feca53314e6623b31f
Santiago Di Camillo <dicamillosantiago@gmail.com>
2023-07-09 16:04:10 -0300

    Avance eliminar medicamento

--- app/src/main/java/andoroid/app/mobiles/notimed/MedicamentoDetalle.kt
+++ app/src/main/java/andoroid/app/mobiles/notimed/MedicamentoDetalle.kt
@@ -6,6 +6,8 @@ import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 
 class MedicamentoDetalle : AppCompatActivity() {
@@ -19,6 +21,8 @@ class MedicamentoDetalle : AppCompatActivity() {
     private fun setup(){
         val editarBtn = findViewById<Button>(R.id.editarBtn)
         val backArrow = findViewById<ImageButton>(R.id.backButton)
+        val borrarBtn = findViewById<ImageButton>(R.id.borrarBtn)
+
         //variables desde el item medicamento
         val id = intent.getStringExtra("id")
         val name = intent.getStringExtra("name")
@@ -41,9 +45,49 @@ class MedicamentoDetalle : AppCompatActivity() {
             intent.putExtra("stock", stock)
             startActivity(intent)
         }
+
         backArrow.setOnClickListener{
             finish()
         }
+
+        //borrar medicamento
+        borrarBtn.setOnClickListener {
+            // Obtén una instancia de FirebaseAuth
+            val firebaseAuth = FirebaseAuth.getInstance()
+
+            // Obtén el usuario autenticado actual
+            val user = firebaseAuth.currentUser
+
+            // Verifica si el usuario está autenticado
+            if (user != null) {
+                val userId = user.uid
+
+                // Crea una referencia a la base de datos
+                val database = FirebaseDatabase.getInstance()
+                val usersRef = database.getReference("users")
+
+                // Obtén una referencia al nodo del usuario en la base de datos
+                val userRef = usersRef.child(userId)
+
+                // Obtén una referencia al nodo del medicamento específico
+                val medicationRef = userRef.child("medicamentos").child(id.toString())
+
+                // Elimina el medicamento de la base de datos
+                medicationRef.removeValue()
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            // El medicamento se ha eliminado exitosamente
+                            println("Medicamento eliminado correctamente")
+                        } else {
+                            // Hubo un error al eliminar el medicamento
+                            println("Error al eliminar el medicamento: ${task.exception?.message}")
+                        }
+                    }
+            }
+
+        }
+
+
 
     }
 }
