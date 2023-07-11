6562ac9f3c0b29e4d0d7d6324535dce62983a0c1
santiagof2 <santiagofritz97@gmail.com>
2023-07-11 01:23:41 -0300

    Hecha la notificacion si el stock del medicamento es bajo

--- app/src/main/java/andoroid/app/mobiles/notimed/AlarmReceiver.kt
+++ app/src/main/java/andoroid/app/mobiles/notimed/AlarmReceiver.kt
@@ -1,89 +1,70 @@
 package andoroid.app.mobiles.notimed
 
+import android.Manifest
+import android.app.Activity
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
+import android.app.TaskStackBuilder
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 
 class AlarmReceiver : BroadcastReceiver() {
+    @OptIn(DelicateCoroutinesApi::class)
     override fun onReceive(context: Context?, intent: Intent?) {
         val id = intent?.getStringExtra("ExtraMessage") ?: return
-        val newIntent = Intent(context, Alarm_Launch::class.java).apply {
-            val database = FirebaseDatabase.getInstance()
-            val currentUser = FirebaseAuth.getInstance().currentUser
-            val uid = currentUser?.uid
-            if (uid != null) {
-                val medicamentoRef = database.getReference("users").child(uid).child("medicamentos").child(id)
+        val database = FirebaseDatabase.getInstance()
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        val uid = currentUser?.uid
+        if (uid != null) {
+            val medicamentoRef = database.getReference("users").child(uid).child("medicamentos").child(id)
 
-                medicamentoRef.addListenerForSingleValueEvent(object : ValueEventListener {
-                    override fun onDataChange(dataSnapshot: DataSnapshot) {
-                        if (dataSnapshot.exists()) {
-                            val stock = dataSnapshot.child("stock").getValue(String::class.java)?.toIntOrNull() ?: 0
-                            val dosis = dataSnapshot.child("dosis").getValue(String::class.java)?.toIntOrNull() ?: 0
-                            val stockActual = stock - dosis
-                            if (stockActual/dosis <= 5){
-                                println("Falta Stock")
-                                enviarNotificacionAsync(context)
-                            }
-                            else{
-                                println("el stock es suficiente")
-                            }
-                            // Actualizar el campo "stock" en Firebase
-                            medicamentoRef.child("stock").setValue(stockActual.toString())
-                                .addOnSuccessListener {
-                                    println("Stock del medicamento actualizado correctamente")
-                                }
-                                .addOnFailureListener { exception ->
-                                    println("Error al actualizar el stock del medicamento: $exception")
-                                }
+            medicamentoRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    if (dataSnapshot.exists()) {
+                        val stock = dataSnapshot.child("stock").getValue(String::class.java)?.toIntOrNull() ?: 0
+                        val dosis = dataSnapshot.child("dosis").getValue(String::class.java)?.toIntOrNull() ?: 0
+                        val stockActual = stock - dosis
+                        val newIntent = Intent(context, Alarm_Launch::class.java)
+                        newIntent.flags= Intent.FLAG_ACTIVITY_NEW_TASK
+                        if (stockActual / dosis <= 5) {
+                            newIntent.putExtra("FaltaStock", "true")
+                        } else {
+                            newIntent.putExtra("FaltaStock", "false")
                         }
+                        val nombre = dataSnapshot.child("name").getValue(String::class.java)
+                        newIntent.putExtra("medicamento", nombre)
+                        context?.startActivity(newIntent)
+                        val stockInt = stockActual.toString()
+                        // Actualizar el campo "stock" en Firebase
+                        medicamentoRef.child("stock").setValue(stockInt)
+                            .addOnSuccessListener {
+                                println("Stock del medicamento actualizado correctamente")
+                            }
+                            .addOnFailureListener { exception ->
+                                println("Error al actualizar el stock del medicamento: $exception")
+                            }
                     }
+                }
 
-                    override fun onCancelled(databaseError: DatabaseError) {
-                        println("Error al obtener el medicamento: $databaseError")
-                    }
-                })
-            }
-
-        }
-        newIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        context?.startActivity(newIntent)
+                override fun onCancelled(databaseError: DatabaseError) {
+                    println("Error al obtener el medicamento: $databaseError")
+                }
+            })
         }
     }
-
-fun enviarNotificacionAsync(context: Context?) {
-    CoroutineScope(Dispatchers.Main).launch {
-        enviarNotificacion(context)
-    }
-}
-
-fun enviarNotificacion(context: Context?) {
-    val notificationManager = context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-    val channelId = "mi_canal"
-    val channelName = "Mi Canal"
-    val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT)
-    notificationManager.createNotificationChannel(channel)
-    val intent = Intent(context, ListaMedicamentos::class.java)
-    val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
-    val notification = NotificationCompat.Builder(context, channelId)
-        .setContentTitle("Alerta")
-        .setContentText("El stock del medicamento es bajo")
-        .setSmallIcon(R.drawable.pill_capsule)
-        .setContentIntent(pendingIntent)
-        .setAutoCancel(true)
-        .build()
-
-    notificationManager.notify(0, notification)
 }
--- app/src/main/java/andoroid/app/mobiles/notimed/Alarm_Launch.kt
+++ app/src/main/java/andoroid/app/mobiles/notimed/Alarm_Launch.kt
@@ -1,5 +1,13 @@
 package andoroid.app.mobiles.notimed
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -7,17 +15,30 @@ import android.media.Ringtone
 import android.media.RingtoneManager
 import android.net.Uri
 import android.widget.Button
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class Alarm_Launch : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.alarm_notify)
-
-        val message = intent.getStringExtra("ExtraMessage")
+        val nombreMed =  intent.getStringExtra("medicamento")
         val textViewMessage: TextView = findViewById(R.id.message)
-        textViewMessage.text = message
+        textViewMessage.text = "Es hora de tomar $nombreMed"
         val cancelBTN: Button = findViewById(R.id.CancelBTN)
         cancelBTN.setOnClickListener{
+            val bajoStock = intent.getStringExtra("FaltaStock")
+            println(nombreMed)
+            println(bajoStock)
+            if (bajoStock == "true" && nombreMed != null){
+                GlobalScope.launch{
+                    enviarNotificacion(this@Alarm_Launch, nombreMed)
+                }
+            }
             onPause()
             finish()
         }
@@ -32,5 +53,41 @@ class Alarm_Launch : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         ringtone?.stop()
+    }
+    private fun enviarNotificacion(context: Context, nombre: String){
+        println("Notificado")
+        val canalID= "canalID"
+        val canalNombre= "bajoStockChannel"
+        crearCanalNotificacion(canalID, canalNombre, context)
+        val resultIntent = Intent(context, ListaMedicamentos::class.java)
+        val resultPendingIntent = TaskStackBuilder.create(context).run{
+            addNextIntentWithParentStack(resultIntent)
+            getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+        }
+        val notificacion = NotificationCompat.Builder(context, canalID).also{
+            it.setContentTitle("Notificacion")
+            it.setContentText("El stock de $nombre es bajo!")
+            it.setSmallIcon(R.drawable.pill_capsule)
+            it.priority = NotificationCompat.PRIORITY_HIGH
+            it.setContentIntent(resultPendingIntent)
+            it.setAutoCancel(true)
+        }.build()
+
+        val notificationManager = NotificationManagerCompat.from(context)
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        notificationManager.notify(0, notificacion)
+    }
+
+    private fun crearCanalNotificacion(canalId: String, canalNombre: String, context: Context){
+        val canalImportancia = NotificationManager.IMPORTANCE_HIGH
+        val canal = NotificationChannel(canalId, canalNombre, canalImportancia)
+        val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        manager.createNotificationChannel(canal)
     }
 }
